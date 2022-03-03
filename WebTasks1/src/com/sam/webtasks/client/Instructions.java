9e0abd21f1deec746bc676be9996524341c36b7f
Sam Gilbert <sam.gilbert@ucl.ac.uk>
2022-03-03 23:28:56 +0000

    v2

--- WebTasks1/src/com/sam/webtasks/client/Instructions.java
+++ WebTasks1/src/com/sam/webtasks/client/Instructions.java
@@ -1,60 +1,113 @@
 package com.sam.webtasks.client;
 
 import com.sam.webtasks.basictools.Counterbalance;
-import com.sam.webtasks.iotask2.IOtask2BlockContext;
 
 public class Instructions {
 
 	public static String Get(int index) {
-		String i="";
-		 
-		switch(index) {
+		String i = "";
+
+		switch (index) {
 		case 0:
-			i="Here are some instructions";
+			i = "In this experiment you will have a simple task to do.<br><br>"
+					+ "You will see several yellow circles inside a box. Inside each circle will be a number. <br><br>"
+					+ "You can move them around with your finger. Your task is to drag them to the bottom of the box in sequence.<br><br>"
+					+ " Please start by dragging 1 all the way to the bottom. This will make it disappear. <br><br>"
+					+ "Then drag 2 to the bottom, then 3, and so on.";
+			break;
+
+		case 1:
+			i="Now you will continue the task with more circles on the screen. ";
+			break;
+			
+		case 2:
+			i="Now you will continue the same task, but sometimes there will be something else to do. <br><br>"
+					+ "As well as dragging each circle in turn to the "
+					+ "bottom of the screen, you will be instructed to drag one or more "
+					+ "of the circles to another part of the box. For instance, you might be told that you should drag number 5 "
+					+ "to the left of the box instead of the bottom.<br><br>"
+					+ "You will still be able to drag any circle to the bottom of the box, but you should try to "
+					+ "remember to drag these special circles to the instructed location. ";
+			break;
+
+		case 3:
+			i="Now you will do the task again, but this time there will be three special circles to remember.";
+			break;
+			
+		case 4:
+			i="Now you will continue with the same task, but sometimes you will be interrupted by "
+					+"an arithmetic question. You will need to answer the question in order to continue the other task.";
+			break;
+			
+		case 5:
+			i="Some people find it helpful to drag "
+					+ "the special circles near to the edge of the box to help them remember.<br><br> "
+					+ "For example, if you had to remember to drag 5 to the left of the box, "
+					+ "you could drag it near to there at the beginning, before you drag the 1. "
+					+ "Then when you eventually got to 5, its location would remind you "
+					+ "what to do. You should feel free to use this strategy if you like, but "
+					+ "it's up to you.<br><br> "
+					+ "Click below to continue.";
+			break;
+			
+		case 6:
+			i = "Now the experiment will start for real.<br><br> "
+					+ "You can choose whether to use the strategy of placing the special circles "
+					+ "at the edge of the box as reminders or just use your own memory without setting any reminders.<br><br>"
+					+ "You should feel free to do the task however you prefer. It's completely up to you.<br><br> "
+					+ "Please click below to continue. ";
+			break;
+			
+		case 7:
+			i = "Now the experiment will start for real.<br><br> "
+					+ "For this part of the experiment, you will have to use your own memory to do the task. "
+					+ "You will not be able to place the special circles at the edge of the box as reminders. "
+					+ "Just use your own memory and try your best.<br><br> "
+					+ "Please click below to continue. ";
+			break;
+			
+		case 8:
+			i = "From this point onwards you will not be able to place special circles at the edge of the box as reminders.<br><br>"
+					+ "Just use your own memory and try your best.<br><br> "
+					+ "Please click below to continue. ";
+			break;
+			
+		case 9:
+			i="For the rest of the experiment, it will be up to you whether to set reminders or not. That is, "
+					+ "you can choose whether to use the strategy of placing the special circles "
+					+ "at the edge of the box as reminders or just use your own memory without setting any reminders.<br><br>"
+					+ "You should feel free to do the task however you prefer. It's completely up to you.<br><br> "
+					+ "Please click below to continue. ";
+			break;
+			
+		case 10:
+			i="TEST ";
 			break;
 		}
 
-		return(i);	
+		return (i);
 	}
-	
+
 	public static String InfoText() {
 		return ("We would like to invite you to participate in this research project. "
-                + "You should only participate if you want to; choosing not to take part "
-                + "will not disadvantage you in any way. Before you decide whether you "
-                + "want to take part, please read the following information carefully and "
-                + "discuss it with others if you wish. Ask us if there is anything that "
-                + "is not clear or you would like more information.<br><br>"
-                + "We are recruiting volunteers from the Amazon Mechanical Turk website to "
-                + "take part in an experiment aiming to improve our understanding of human "
-                + "attention and memory. You will see various objects on the screen like coloured, numbered circles, "
-                + "and you will be asked to move them with your computer mouse. Sometimes you will be asked to remember "
-                + "particular numbers and move the corresponding circle in a particular direction. You will be asked how "
-                + "confident you are in your ability to solve the task. "
-                + "The experiment "
-                + "will last approximately 45 minutes and you will receive a payment of $2 plus an additional bonus via the "
-                + "Amazon Mechanical Turk payment system. There are no anticipated risks or "
-                + "benefits associated with participation in this study.<br><br>"
-                + "It is up to you to decide whether or not to take part. If you choose "
-                + "not to participate, you won't incur any penalties or lose any "
-                + "benefits to which you might have been entitled. However, if you do "
-                + "decide to take part, you can print out this information sheet and "
-                + "you will be asked to fill out a consent form on the next page. "
-                + "Even after agreeing to take "
-                + "part, you can still withdraw at any time and without giving a reason. If you withdraw before the "
-                + "end of the experiment, we will not retain your data and it will not be analysed."
-                + "<br><br>All data will be collected and stored in accordance with the General Data Protection "
-                + "Regulations 2018. Personal information is stored separately from test results, and researchers "
-                + "on this project have no access to this data. Your personal information such as name and email "
-                + "address is held by Amazon Mechanical Turk but the researchers on this project have no acccess "
-                + "to this. Data from this experiment may be made available to the research community, for example by "
-                + "posting them on websites such as the Open Science Framework (<a href=\"http://osf.io\">http://osf.io</a>). "
-                + "It will not be possible to identify you from these data.<br><br>"
-                + "We aim to publish the results of this project in scientific journals and book chapters. Copies of the "
-                + "results can either be obtained directly from the scientific journals' websites or from us.<br><br>"
-                + "Should you wish to raise a complaint, please contact the Principal Investigator of this project, "
-                + "Dr Sam Gilbert (<a href=\"mailto:sam.gilbert@ucl.ac.uk\">sam.gilbert@ucl.ac.uk</a>). However, "
-                + "if you feel your complaint has not been handled to your satisfaction, please be aware that you can "
-                + "also contact the Chair of the UCL Research Ethics Committee (<a href=\"mailto:ethics@ucl.ac.uk\">ethics@ucl.ac.uk</a>).");
-    }
+				+ "You should only participate if you want to; choosing not to take part "
+				+ "will not disadvantage you in any way. Before you decide whether you "
+				+ "want to take part, please read the following information carefully and "
+				+ "discuss it with others if you wish. Ask us if there is anything that "
+				+ "is not clear or you would like more information.<br><br>"
+				+ "We are recruiting volunteers from the Amazon Mechanical Turk website to "
+				+ "take part in an experiment aiming to improve our understanding of human "
+				+ "attention and memory. Full instructions will be provided before the experiment begins. "
+				+ "The experiment " + "will last approximately 40 minutes. There are no anticipated risks or "
+				+ "benefits associated with participation in this study.<br><br>"
+				+ "It is up to you to decide whether or not to take part. If you choose "
+				+ "not to participate, you won't incur any penalties or lose any "
+				+ "benefits to which you might have been entitled. However, if you do "
+				+ "decide to take part, you can print out this information sheet and "
+				+ "you will be asked to fill out a consent form on the next page. " + "Even after agreeing to take "
+				+ "part, you can still withdraw at any time and without giving a reason. "
+				+ "<br><br>All data will be collected and stored in accordance with the UK Data "
+				+ "Protection Act 1998.");
+	}
 
 }
--- WebTasks1/src/com/sam/webtasks/client/SequenceHandler.java
+++ WebTasks1/src/com/sam/webtasks/client/SequenceHandler.java
@@ -18,17 +18,11 @@
 //SequenceHandler.SetLoop(0,false) will switch to the main loop,
 //continuing from where we left off.
 
-//TODO:
-//scroll
-//data output
-//resume where you left off
-
 package com.sam.webtasks.client;
 
 import java.util.ArrayList;
 
 import com.google.gwt.user.client.Window;
-import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.sam.webtasks.basictools.CheckIdExists;
 import com.sam.webtasks.basictools.CheckScreenSize;
@@ -44,15 +38,11 @@ import com.sam.webtasks.basictools.Slider;
 import com.sam.webtasks.basictools.TimeStamp;
 import com.sam.webtasks.iotask1.IOtask1Block;
 import com.sam.webtasks.iotask1.IOtask1BlockContext;
+import com.sam.webtasks.iotask1.IOtask1DisplayParams;
 import com.sam.webtasks.iotask1.IOtask1InitialiseTrial;
 import com.sam.webtasks.iotask1.IOtask1RunTrial;
-import com.sam.webtasks.iotask2.IOtask2Block;
-import com.sam.webtasks.iotask2.IOtask2BlockContext;
-import com.sam.webtasks.iotask2.IOtask2RunTrial;
-import com.sam.webtasks.perceptualTask.PerceptBlock;
-import com.sam.webtasks.timeBasedOffloading.TimeBlock;
-import com.sam.webtasks.iotask2.IOtask2InitialiseTrial;
-import com.sam.webtasks.iotask2.IOtask2PreTrial;
+
+
 
 public class SequenceHandler {
 	public static void Next() {	
@@ -64,15 +54,241 @@ public class SequenceHandler {
 			switch (sequencePosition.get(0)) {
 			/***********************************************************************
 			 * The code here defines the main sequence of events in the experiment *
-			 **********************************************************************/
+			 ********************************************************************/			
 			case 1:
 				ClickPage.Run(Instructions.Get(0), "Next");
 				break;
 			case 2:
+				//practice 1: 6 circles, no targets
+				IOtask1Block block1 = new IOtask1Block();
+				block1.blockNum = 1;
+				block1.nCircles = 6;
+				block1.nTrials = 1;
+				block1.nTargets = 0;
+				block1.askArithmetic = false;
+				block1.offloadCondition = Names.REMINDERS_NOTALLOWED;		
+				block1.Run();
+				break;
+			case 3:
+				ClickPage.Run(Instructions.Get(1), "Next");
+				break;
+			case 4:
+				//practice 2: 10 circles, no targets
+				IOtask1Block block2 = new IOtask1Block();
+				block2.blockNum = 2;
+				block2.nTrials = 1;
+				block2.nTargets = 0;
+				block2.askArithmetic = false;
+				block2.offloadCondition = Names.REMINDERS_NOTALLOWED;		
+				block2.Run();
+				break;
+			case 5:
+				ClickPage.Run(Instructions.Get(2), "Next");
+				break;
+			case 6:
+				//practice 3: 10 circles, 1 target
+				IOtask1Block block3 = new IOtask1Block();
+				block3.blockNum = 3;
+				block3.nTrials = 1;
+				block3.nTargets = 1;
+				block3.askArithmetic = false;
+				block3.offloadCondition = Names.REMINDERS_NOTALLOWED;		
+				block3.Run();				
+				break;
+			case 7:
+				ClickPage.Run(Instructions.Get(4), "Next");
+				break;
+			case 8:
+				//practice 4: 10 circles, 1 target with interruption
+				IOtask1Block block4 = new IOtask1Block();
+				block4.blockNum = 4;
+				block4.nTrials = 1;
+				block4.nTargets = 1;
+				block4.askArithmetic = true;
+				block4.offloadCondition = Names.REMINDERS_NOTALLOWED;		
+				block4.Run();
+				break;				
+			case 9:
+				// Metacognitive Judgement unaided memory 1 target
+				Slider.Run("Now that you have had some practice with the experiment, we would like you to tell us "
+						+ "how accurately you can perform the task. Please ignore the earlier practice trials and "
+						+ "just tell us how accurately you can do the task when it is the same difficulty as the "
+						+ "trial you have just completed, with one special circle to remember. <br><br>"
+						+ "Please use the scale below to indicate what percentage "
+						+ "of the special circles you can correctly drag to the instructed side of the square, on average. "
+						+ "100% would mean that you always get every single one correct. 0% would mean that you can "
+						+ "never get any of them correct. ", "Never", "Always");
+				break;
+			case 10:
+				//save the selected slider value to the database
+				PHP.logData("sliderValue",  "" + Slider.getSliderValue(), true);
+				break;
+			case 11:
+				ClickPage.Run(Instructions.Get(3), "Next");
+				break;
+			case 12:
+				//practice 5: 10 circles, 3 targets with interruption
+				IOtask1Block block5 = new IOtask1Block();
+				block5.blockNum = 5;
+				block5.nTrials = 1;
+				block5.nTargets = 3;
+				block5.askArithmetic = true;
+				block5.offloadCondition = Names.REMINDERS_NOTALLOWED;		
+				block5.Run();
+				break;				
+			case 13:
+				// Metacognitive Judgement unaided memory 3 targets
+				Slider.Run("Please ignore the earlier practice trials and "
+						+ "just tell us how accurately you can do the task when it is the same difficulty as the "
+						+ "trial you have just completed, with three special circles to remember. <br><br>"
+						+ "Please use the scale below to indicate what percentage "
+						+ "of the special circles you can correctly drag to the instructed side of the square, on average. "
+						+ "100% would mean that you always get every single one correct. 0% would mean that you can "
+						+ "never get any of them correct. ", "Never", "Always");
+				break;
+			case 14:
+				//save the selected slider value to the database
+				PHP.logData("sliderValue",  "" + Slider.getSliderValue(), true);
+				break;
+			case 15:
+				ClickPage.Run(Instructions.Get(5), "Next");
+				break;
+			case 16:
+				//practice 6: 10 circles, 1 target, optional reminders
+				IOtask1Block block6 = new IOtask1Block();
+				block6.blockNum = 6;
+				block6.nTrials = 1;
+				block6.nTargets = 1;
+				block6.askArithmetic = true;
+				block6.offloadCondition = Names.REMINDERS_OPTIONAL;	
+				block6.Run();
+				break;
+			case 17:
+				// Metacognitive Judgement optional reminders 1 target
+				Slider.Run("Please ignore the earlier practice trials and "
+						+ "just tell us how accurately you can do the task when it is the same difficulty as the "
+						+ "trial you have just completed, with one special circle to remember and the option "
+						+ "to set reminders by dragging them to the edge of the box. <br><br>"
+						+ "Please use the scale below to indicate what percentage "
+						+ "of the special circles you can correctly drag to the instructed side of the square, on average. "
+						+ "100% would mean that you always get every single one correct. 0% would mean that you can "
+						+ "never get any of them correct. ", "Never", "Always");
+				break;
+			case 18:
+				//save the selected slider value to the database
+				PHP.logData("sliderValue",  "" + Slider.getSliderValue(), true);
+				break;
+			case 19:
+				ClickPage.Run(Instructions.Get(3), "Next");
+				break;
+			case 20:
+				//practice 7: 10 circles, 3 targets, optional reminders
+				IOtask1Block block7 = new IOtask1Block();
+				block7.blockNum = 7;
+				block7.nTrials = 1;
+				block7.nTargets = 3;
+				block7.askArithmetic = true;
+				block7.offloadCondition = Names.REMINDERS_OPTIONAL;	
+				block7.Run();
+				break;
+			case 21:
+				// Metacognitive Judgement optional reminders 3 targets
+				Slider.Run("Please ignore the earlier practice trials and "
+						+ "just tell us how accurately you can do the task when it is the same difficulty as the "
+						+ "trial you have just completed, with three special circles to remember and the option "
+						+ "to set reminders by dragging them to the edge of the box. <br><br>"
+						+ "Please use the scale below to indicate what percentage "
+						+ "of the special circles you can correctly drag to the instructed side of the square, on average. "
+						+ "100% would mean that you always get every single one correct. 0% would mean that you can "
+						+ "never get any of them correct. ", "Never", "Always");
+				break;
+			case 22:
+				//save the selected slider value to the database
+				PHP.logData("sliderValue",  "" + Slider.getSliderValue(), true);
+				break;
+			case 23:
+				//instructions phase 1
+				
+				//Window.alert("test");
+
+				if (Counterbalance.getFactorLevel("conditionOrder") == ExtraNames.OFFLOAD_SECOND){
+					ClickPage.Run(Instructions.Get(7), "Next");
+				}
+				if (Counterbalance.getFactorLevel("conditionOrder") == ExtraNames.OFFLOAD_FIRST){
+					ClickPage.Run(Instructions.Get(6), "Next");
+				}
+				
+				break;				
+			case 24:
+				//phase 1
+				IOtask1Block block8 = new IOtask1Block();
+				block8.blockNum = 8;
+				block8.nTrials = 10;
+				block8.targetList.add(1);
+				block8.targetList.add(3);
+				block8.targetList.add(1);
+				block8.targetList.add(3);
+				block8.targetList.add(1);
+				block8.targetList.add(3);
+				block8.targetList.add(1);
+				block8.targetList.add(3);
+				block8.targetList.add(1);
+				block8.targetList.add(3);				
+				block8.askArithmetic = true;
+				if (Counterbalance.getFactorLevel("conditionOrder") == ExtraNames.OFFLOAD_SECOND) {
+					block8.offloadCondition = Names.REMINDERS_NOTALLOWED;}
+				if (Counterbalance.getFactorLevel("conditionOrder") == ExtraNames.OFFLOAD_FIRST) {
+					block8.offloadCondition = Names.REMINDERS_OPTIONAL;}
+				block8.Run();
+				break;
+			case 25:
+				//instructions phase 2
+				if (Counterbalance.getFactorLevel("conditionOrder") == ExtraNames.OFFLOAD_SECOND) {
+					ClickPage.Run(Instructions.Get(9), "Next");
+				}
+				
+				if (Counterbalance.getFactorLevel("conditionOrder") == ExtraNames.OFFLOAD_FIRST) {
+					ClickPage.Run(Instructions.Get(8), "Next");
+				}
+				break;
+			case 26:
+				//phase 2
+				IOtask1Block block9 = new IOtask1Block();
+				block9.blockNum = 9;
+				block9.nTrials = 10;
+				block9.targetList.add(1);
+				block9.targetList.add(3);
+				block9.targetList.add(1);
+				block9.targetList.add(3);
+				block9.targetList.add(1);
+				block9.targetList.add(3);
+				block9.targetList.add(1);
+				block9.targetList.add(3);
+				block9.targetList.add(1);
+				block9.targetList.add(3);
+				block9.askArithmetic = true;
+				if (Counterbalance.getFactorLevel("conditionOrder") == ExtraNames.OFFLOAD_SECOND) {
+					block9.offloadCondition = Names.REMINDERS_OPTIONAL;}
+				if (Counterbalance.getFactorLevel("conditionOrder") == ExtraNames.OFFLOAD_FIRST) {
+					block9.offloadCondition = Names.REMINDERS_NOTALLOWED;}
+				block9.Run();
+				break;
+			case 27:
+				// log data and check that it saves
+				String data = Counterbalance.getFactorLevel("conditionOrder") + ",";
+				data = data + SessionInfo.gender + ",";
+				data = data + SessionInfo.age + ",";
+				data = data + TimeStamp.Now();
+
+				PHP.logData("finish", data, true);
+				break;
+			case 28:
+				// complete the experiment
 				Finish.Run();
 				break;
 			}
 			break;
+		
 
 		/********************************************/
 		/* no need to edit the code below this line */
@@ -122,19 +338,12 @@ public class SequenceHandler {
 				}
 				break;
 			case 8:
-				//record the participant's counterbalancing condition in the status table				
-				if (!SessionInfo.resume) {
-					PHP.UpdateStatus("" + Counterbalance.getCounterbalancingCell() + ",1,0,0,0,0");
-				} else {
-					SequenceHandler.Next();
-				}
-				break;
-			case 9:
 				SequenceHandler.SetLoop(0, true); // switch to and initialise the main loop
 				SequenceHandler.Next(); // start the loop
 				break;
 			}
 			break;
+			
 		case 2: // IOtask1 loop
 			switch (sequencePosition.get(2)) {
 			/*************************************************************
@@ -165,59 +374,10 @@ public class SequenceHandler {
 				SequenceHandler.SetLoop(2, true);
 				SequenceHandler.Next();
 				break;
-			}
-			break;
-		case 3: //IOtask2 loop
-			switch (sequencePosition.get(3)) {
-			/*************************************************************
-			 * The code here defines the sequence of events in subloop 3 *
-			 * This runs a single trial of IOtask2                       *
-			 *************************************************************/
-			case 1:
-				// first check if the block has ended. If so return control to the main sequence
-				// handler
-				IOtask2Block block = IOtask2BlockContext.getContext();
+				// TODO: mechanism to give post-trial feedback?
 				
-				if (block.currentTrial == block.nTrials) {
-					SequenceHandler.SetLoop(0,  false);
-				}
-				
-				SequenceHandler.Next();
-				break;
-			case 2:
-				IOtask2InitialiseTrial.Run();
-				break;
-			case 3:;
-				//present the pre-trial choice if appropriate
-				if (IOtask2BlockContext.currentTargetValue() > -1) {
-					IOtask2PreTrial.Run();
-				} else { //otherwise just skip to the start of the block
-					if ((IOtask2BlockContext.getTrialNum() > 0)&&(IOtask2BlockContext.countdownTimer())) {
-						//if we're past the first trial and there's a timer, click to begin
-						ClickPage.Run("Ready?", "Continue");
-					} else {
-						SequenceHandler.Next();
-					}
-				}
-				break;
-			case 4:
-				if (IOtask2BlockContext.getNTrials() == -1) { //if nTrials has been set to -1, we quit before running
-					SequenceHandler.SetLoop(0,  false);
-					SequenceHandler.Next();
-				} else {
-					//otherwise, run the trial
-					IOtask2RunTrial.Run();
-				}
-				break;
-			case 5:
-				IOtask2PostTrial.Run();
-				break;
-			case 6:
-				//we have reached the end, so we need to restart the loop
-				SequenceHandler.SetLoop(3,  true);
-				SequenceHandler.Next();
-				break;
 			}
+		
 		}
 	}
 	
@@ -236,24 +396,19 @@ public class SequenceHandler {
 			sequencePosition.set(whichLoop, 0);
 		}
 	}
+
+	// set a new position
+	public static void SetPosition(int newPosition) {
+		sequencePosition.set(whichLoop, newPosition);
+	}
 	
 	// get current loop
 	public static int GetLoop() {
 		return (whichLoop);
 	}
 
-	// set a new position
-	public static void SetPosition(int newPosition) {
-		sequencePosition.set(whichLoop, newPosition);
-	}
-
 	// get current position
 	public static int GetPosition() {
 		return (sequencePosition.get(whichLoop));
-	}
-	
-	// get current position from particular loop
-	public static int GetPosition(int nLoop) {
-		return (sequencePosition.get(nLoop));
 	}
 }
